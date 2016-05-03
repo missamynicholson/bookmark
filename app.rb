65c29cd93e96c84899a1e4a6d961abfdcb11ac99
Robin Heathcote <robin.heathcote@icloud.com>
2016-05-03 17:44:20 +0100

    Challenge: 9 - Created model for Link

--- app.rb
+++ app.rb
@@ -1,9 +1,10 @@
 require 'sinatra/base'
+require './models/link'
 
 class Bookmark < Sinatra::Base
-  get '/' do
-    'Hello bookmark!'
-  end
+  # get '/' do
+  #   'Hello bookmark!'
+  # end
 
   # start the server if ruby file executed directly
   run! if app_file == $0
--- spec/features/list_links_spec.rb
+++ spec/features/list_links_spec.rb
@@ -4,7 +4,6 @@ feature 'List the links' do
 	scenario 'show a list of links' do
     link = Link.create(:url => 'http://examkfdl', :title => 'example')
     visit '/links'
-    expect(page.status_code).to eq 200
     expect(page).to have_content 'http://examkfdl'
   end
 end
