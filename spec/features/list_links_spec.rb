b49dbb1a11648368789d0d582dd405a9e943492e
Amy Nicholson <missamynicholson@gmail.com>
2016-05-03 17:10:39 +0100

    First user story feature test fail

---
+++ spec/features/list_links_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper.rb'
+
+feature 'List the links' do
+	scenario 'show a list of links' do
+    link = Link.create(:url => 'http://examkfdl', :title => 'example')
+    visit '/links'
+    expect(page.status_code).to eq 200
+    expect(page).to have_content 'http://examkfdl'
+  end
+end
